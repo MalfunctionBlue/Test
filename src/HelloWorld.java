3b21a153ed23e75a0fa0cda45ce389ac156ed5cc
mzhang62 <mzhang62@ucsc.edu>
2018-04-30 23:29:08 -0700

    very first hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+Name: Michael Zhang & David Yu
+Note: testing for our very first GitHub project
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
