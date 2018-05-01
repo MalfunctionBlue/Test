ad51252be9a9f6e97520cd53ea4caf250beed438
David Yu <dyu25@ucsc.edu>
2018-05-01 15:30:19 -0700

    testing HelloWorld2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ Note: testing for our very first GitHub project
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! ");
+        System.out.println("test2!");
     }
 }
